5ebd2c2f253732233f462eaa4fe40af7e3ccfcae
KhotsoSelading <k.selading007@gmail.com>
2023-09-20 16:10:08 +0200

    Databases

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,15 +1,15 @@
--- a script that prepares a MySQL server for the project
--- setup database
+-- a script that prepares the MYSQL Server
+-- sets up the database
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
--- creates new user hbnb_dev
+-- creates new user hbnb_dev in the database
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
--- grant basic usage
+-- grant basic usage to the database
 GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
 
--- grant select privelges on performance shema
+-- grant select privelges on performance shema in the database
 GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
 
--- grant all privileges on hbnbh_dev
+-- grant all privileges on hbnbh_dev database
 GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,16 +1,16 @@
--- a script that prepares a MySQL server for the project
--- setup database
+-- a script that prepares the MYSQL Server
+-- sets up the database
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 
--- creates new user hbnb_test
+-- creates new user hbnb_dev in the database
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost'
 IDENTIFIED BY 'hbnb_test_pwd';
 
--- grant basic usage
+-- grant basic usage to the database
 GRANT USAGE ON *.* TO 'hbnb_test'@'localhost';
 
--- grant select privelges on performance shema
+-- grant select privelges on performance shema in the database
 GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
 
--- grant all privileges on hbnbh_test
+-- grant all privileges on hbnbh_dev database
 GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
