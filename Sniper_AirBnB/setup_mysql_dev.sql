a53a62c0cd0455d02f72b2d9d4f39f16f2023924
KhotsoSelading <k.selading007@gmail.com>
2023-09-20 18:27:10 +0200

    Databases

--- Sniper_AirBnB/setup_mysql_dev.sql
+++ Sniper_AirBnB/setup_mysql_dev.sql
@@ -1,15 +1,15 @@
--- a script that prepares a MySQL server for the project
--- setup database
+-- A script that prepares a MySQL server AirBnB_clone_v2
+-- SQL Statement to setup database
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
--- creates new user hbnb_dev
+-- SQL Statement to create new user hbnb_dev
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
--- grant basic usage
+-- SQL Statement to grant basic usage
 GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
 
--- grant select privelges on performance shema
+-- SQL Statement to grant select privelges on performance shema
 GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
 
--- grant all privileges on hbnbh_dev
+-- SQL Statement to grant all privileges on hbnbh_dev
 GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
--- Sniper_AirBnB/setup_mysql_test.sql
+++ Sniper_AirBnB/setup_mysql_test.sql
@@ -1,16 +1,16 @@
--- a script that prepares a MySQL server for the project
--- setup database
+-- A script that prepares a MySQL server AirBnB_clone_v2
+-- SQL Statement to setup database
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 
--- creates new user hbnb_test
+-- SQL Statement to create new user hbnb_test
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost'
 IDENTIFIED BY 'hbnb_test_pwd';
 
--- grant basic usage
+-- SQL Statement to grant basic usage
 GRANT USAGE ON *.* TO 'hbnb_test'@'localhost';
 
--- grant select privelges on performance shema
+-- SQL Statement to grant select privelges on performance shema
 GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
 
--- grant all privileges on hbnbh_test
+-- SQL Statement to grant all privileges on hbnbh_test
 GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
